89f25a50294401519d5128dc73f2afc422004b27
GAUTAMVERM <89487056+GAUTAMVERM@users.noreply.github.com>
2025-04-13 13:46:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call Rectangle constructor with same side for width and height
+  }
+
+  getPerimeter() {
+    return this._width * 4;
+  }
+}
+
+
+
+// Do not change the code below
 window.Rectangle = Rectangle;
 window.Square = Square;
